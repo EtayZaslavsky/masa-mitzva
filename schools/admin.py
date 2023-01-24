41a3e7d36e4cdd82181cb255764a8919943bb3b2
etay <etay@zaslavsky.com>
2023-01-24 18:10:40 +0200

    add nested_admin app and created a school nested inline admin.

--- schools/admin.py
+++ schools/admin.py
@@ -1,8 +1,22 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group
+from nested_admin import NestedTabularInline, NestedModelAdmin
 from .models import School, Grade, ClassGroup
 
-admin.site.register(School)
+
+class ClassGroupInline(NestedTabularInline):
+    model = ClassGroup
+
+class GradeInline(NestedTabularInline):
+    model = Grade
+    inlines = [ClassGroupInline]
+
+class SchoolAdmin(NestedModelAdmin):
+    inlines = [GradeInline]
+
+
+admin.site.register(School, SchoolAdmin)
 admin.site.register(Grade)
 admin.site.register(ClassGroup)
+
 admin.site.unregister(Group)
